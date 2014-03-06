adce4be84bfc64f68eaff9ab6912081ed6f82181
Jefferson Peak <jgpeak@gmail.com>
2014-03-06 20:58:41 +0000

    Base Setup and Javascript layout commented

---
+++ Client_Files/custom_olapic_config.js
@@ -0,0 +1,9 @@
+//TO-DO: Create root Olapic Object for everything to be assigned to in order to prevent clouding of name space
+	//TO-DO: Create Object for Carousel assign to Olapic
+		//TO-DO: Create Function to Create Carousel based on an array
+		//TO-DO: Create Function to Handle Carousel Movement
+		//TO-DO: Create Function to Handle light box image view
+	//TO-DO: Create Object for handling Olapic API Calls assign to Olapic
+		//TO-DO: Create Function for making the API Call
+		//TO-DO: Create Function for Processing the return Object
+	//TO-DO: Create init function.
