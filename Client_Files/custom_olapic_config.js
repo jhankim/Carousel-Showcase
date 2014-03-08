86be172ea31237691ee75d9c5e18cad69bdac6d1
Jefferson Peak <jgpeak@gmail.com>
2014-03-08 16:44:59 +0000

    Added function to process API result, added function to manage creating the carousel using bxslider library

--- Client_Files/custom_olapic_config.js
+++ Client_Files/custom_olapic_config.js
@@ -1,6 +1,7 @@
 //Olapic Carousel constructor
 var olapicCarousel = olapicCarousel || function(divID) {
-	this.targetDiv = divID;
+	this.targetDiv = divID || 'olapicCarousel';
+	this.imageArray = [];
 
 	this.init();
 };
@@ -36,7 +37,7 @@ olapicCarousel.prototype.log = function(type, message, argsArray){
 	}
 };
 
-//Function for making the API Call
+//Function for making the API Call to Olapic to get Images
 olapicCarousel.prototype.api = function(callback){
 	var apiKey = '0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18'
 		, mediaEndpoint = 'https://photorankapi-a.akamaihd.net/customers/215757/media/recent'
@@ -55,22 +56,84 @@ olapicCarousel.prototype.api = function(callback){
         		callback(e.data._embedded);
         	}
         	else {
-        		that.log('error', 'No callback defined for API call');
+        		that.log('error', 'No callback defined for API call.');
         	}
         },  
 		error: function(msg) {
-			that.log('error', 'API call failed' , [msg])
+			that.log('error', 'API call failed.' , [msg])
 		}
      });
 };
+
+//TO-DO: Create Function to process API response to Carousel Array
+olapicCarousel.prototype.processApiResponse = function(apiArray, imageArray, callback){
+	for(var i in apiArray){
+		var imageObject = {};
+
+		imageObject.identifier = apiArray[i]._analytics.oid;
+		imageObject.captions = apiArray[i].caption;
+		imageObject.images = apiArray[i].images;
+
+		imageArray.push(imageObject);
+	}
+
+	if(callback) callback();
+};
+
 //TO-DO: Create Function to Create Carousel based on an array
-//TO-DO: Create Function to Handle Carousel Movement
+olapicCarousel.prototype.createCarousel = function(targetDiv, imageArray){
+	if(!imageArray) this.log('error', 'No imageArray passed to createCarousel.')
+
+	var carouselHTML = ''
+		, openingHTML = '<ul class="bxslider">'
+		, closingHTML = '</ul>'
+		, imageURL = ''
+		, imageTitle = '';
+
+	carouselHTML += openingHTML;
+
+	for(i in imageArray){
+		imageURL = imageArray[i].images.square;
+		imageTitle = imageArray[i].captions;
+
+		carouselHTML += '<li><img src="' + imageURL + '" title="' + imageTitle + '" /></li>';
+	}
+
+	carouselHTML += closingHTML;
+
+	var that = this;
+
+	$(function() {
+		that.log('log', 'Created Carousel targetDiv', [targetDiv]);
+		that.log('log', 'Created Carousel HTMl', [carouselHTML]);
+	    $(targetDiv).html(carouselHTML);
+
+	    $('.bxslider').bxSlider({
+			minSlides: 1,
+			maxSlides: 5,
+			slideWidth: 90,
+			slideMargin: 5
+		});
+	});
+};
+
 //TO-DO: Create Function to Handle light box image view
+
 //TO-DO: Create init function.
 olapicCarousel.prototype.init = function(){
+	var imageArray = this.imageArray
+		, targetDiv = this.targetDiv
+		, that = this;
 	//TEMP: Test API function
-	this.api(function(e){console.log(e);});
+	this.api(function(apiArray){
+		that.log('log', 'Raw apiArray', [apiArray]);
+		that.processApiResponse(apiArray, imageArray, function(){
+			that.log('log', 'Processed imageArray', [imageArray]);
+			that.log('log', 'Create Carousel in targetDiv', [targetDiv])
+			that.createCarousel(targetDiv, imageArray);
+		})
+	});
 };
 
 
-var test = new olapicCarousel();
+var test = new olapicCarousel('#carousel1');
